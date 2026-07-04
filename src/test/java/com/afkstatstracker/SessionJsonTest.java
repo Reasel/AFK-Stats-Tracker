f893770dd2c46c07d7a21543b248527b2ded7ba5
Reasel <reasel@pm.me>
2026-07-03 21:20:08 -0700

    Simplify SessionJsonTest JSON parsing

--- src/test/java/com/afkstatstracker/SessionJsonTest.java
+++ src/test/java/com/afkstatstracker/SessionJsonTest.java
@@ -5,8 +5,6 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import org.junit.Test;
 
-import java.io.StringReader;
-
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
@@ -16,7 +14,7 @@ public class SessionJsonTest
 	public void sessionSerializesWithWebsiteContractFieldNames()
 	{
 		Session session = new Session("id-1", "Test", 1000L, 61000L, 42, 87L, 1500.5, 12.3);
-		JsonObject json = new JsonParser().parse(new StringReader(new Gson().toJson(session))).getAsJsonObject();
+		JsonObject json = new JsonParser().parse(new Gson().toJson(session)).getAsJsonObject();
 
 		assertTrue(json.has("name"));
 		assertTrue(json.has("startTime"));
